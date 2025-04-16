6943bddf8ed02804b3c0859c2a7fda66176fce44
Jamal Ehsanpour <98238613+jamal-ehsanpour@users.noreply.github.com>
2025-04-16 18:20:17 +0200

    Update install-httpd.sh

--- beginner/ec2-web-server/install-httpd.sh
+++ beginner/ec2-web-server/install-httpd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Use this for your user data (script from top to bottom)
 # install httpd (Linux 2 version)
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>This Is My First Web Server Using Terrafom :) $(hostname -f)</h1>" > /var/www/html/index.html
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo "<h1>This Is My First Web Server Using Terrafom :) $(hostname -f)</h1>" | sudo tee /var/www/html/index.html
